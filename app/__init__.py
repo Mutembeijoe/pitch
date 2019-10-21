0fa5f4dcc413904292f32008e2d4f47b0c51952c
mutembeijoe <mutembeijoe@gmail.com>
2019-10-22 00:04:40 +0300

    adds dummy pitch data to homepage, creates a user registration form class, its template and validation checks and errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '27680ccbcade6026e338d4349503c3ad'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,33 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, url_for, redirect
+from app.forms import RegistrationForm
 
-
+pitches = [
+    {
+        'author': 'James Dean',
+        'title': 'Pitch 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'category' :'Technology'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Pitch 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'category': 'Biology'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', pitches = pitches)
+
+@app.route('/register', methods =['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form = form)
