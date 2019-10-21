9a3a7c74ff559e8f9f14e999829625754403edd0
mutembeijoe <mutembeijoe@gmail.com>
2019-10-22 00:15:28 +0300

    installs flask SQlALCHEMY, configures a db instance and path to SQlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '27680ccbcade6026e338d4349503c3ad'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 from app import routes
