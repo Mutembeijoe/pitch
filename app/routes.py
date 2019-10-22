7a402e992a7b09c412e65bd8f3dad642634f8dd0
mutembeijoe <mutembeijoe@gmail.com>
2019-10-22 10:39:41 +0300

    protects the account page from un-auntheticated users by sending them to the login page and on successful login their are redirected back to their accounts page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, bcrypt, db
-from flask import render_template, flash, url_for, redirect
+from flask import render_template, flash, url_for, redirect, request
 from app.forms import RegistrationForm, LoginForm
-from flask_login import login_user,current_user, logout_user
+from flask_login import login_user,current_user, logout_user, login_required
 from app.models import User
 
 pitches = [
@@ -50,7 +50,8 @@ def login():
         print(user)
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -60,3 +61,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
