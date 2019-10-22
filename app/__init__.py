a9b262b5fc05c4f06830315a14e0c0fdd52875cf
mutembeijoe <mutembeijoe@gmail.com>
2019-10-22 07:35:06 +0300

    modifies folder structure, removes db models from __init__.py file of app to models.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 
 app = Flask(__name__)
@@ -9,42 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__='users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    pitches = db.relationship('Pitch', backref='author', lazy=True)
-    comments = db.relationship('Comment', backref='author', lazy= True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Pitch(db.Model):
-    __tablename__='pitches'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    upvote = db.Column(db.Integer, nullable=False, default = 0)
-    downvote = db.Column(db.Integer, nullable=False, default = 0)
-    comments = db.relationship('Comment', backref='pitch', lazy=True)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-class Comment(db.Model):
-    __tablename__ ='comments'
-    id = db.Column(db.Integer, primary_key=True)
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return f"Comment : id: {self.id} comment: {self.description}"
 
 from app import routes
