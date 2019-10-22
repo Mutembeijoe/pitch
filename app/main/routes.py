b01fbab88faae8de96600d487d7805b447c497a3
mutembeijoe <mutembeijoe@gmail.com>
2019-10-22 23:14:54 +0300

    adds a side nav in the gome page to toggle between categories

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,7 +6,9 @@ main = Blueprint('main',__name__)
 
 @main.route('/')
 @main.route('/home')
-def home():
+@main.route('/home/<string:category>')
+def home(category = None):
+    print(category)
     pitches = Pitch.query.all()
     return render_template('home.html', pitches = pitches)
 
